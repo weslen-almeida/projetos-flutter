d3fc9811a89ab1e2f4528f1032f7acffa1cb1185
Weslen Almeida <weslengomes@gmail.com>
2025-02-04 06:36:53 -0300

    feat(calc): criando a primeira pagina

---
+++ calculator/lib/src/calc_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CalcPage extends StatelessWidget {
+  const CalcPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
