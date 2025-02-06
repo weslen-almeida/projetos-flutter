20f38dbbbff40b3769d06a8bd28df9ccd2ef9189
Weslen Almeida <weslengomes@gmail.com>
2025-02-06 06:19:11 -0300

    feat(pages): criando pagina de butões e display

--- calculator/lib/src/calc_page.dart
+++ calculator/lib/src/calc_page.dart
@@ -1,10 +1,30 @@
+import 'package:calculator/src/widgets/button_hub.dart';
+import 'package:calculator/src/widgets/display.dart';
 import 'package:flutter/material.dart';
 
-class CalcPage extends StatelessWidget {
+class CalcPage extends StatefulWidget {
   const CalcPage({super.key});
 
-  @override
+ @override
+ State<CalcPage> createState() => _CalcPageState();
+}
+
+class _CalcPageState extends State<CalcPage> {
+   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Material(
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child:  Display(value: '100')
+          ),
+          Expanded(
+            flex: 3,
+            child: ButtonHub(),
+            ),
+        ],
+      ),
+    );
   }
 }
