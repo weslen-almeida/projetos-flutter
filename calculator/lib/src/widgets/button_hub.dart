c28a70ec8f98040745658cb6f46f78696a2626a8
Weslen Almeida <weslengomes@gmail.com>
2025-02-19 06:17:21 -0300

    feat(pages): inserindo os botões da calculadora

--- calculator/lib/src/widgets/button_hub.dart
+++ calculator/lib/src/widgets/button_hub.dart
@@ -6,43 +6,44 @@ class ButtonHub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.count(
-  primary: false,
-  padding: const EdgeInsets.all(20),
-  crossAxisSpacing: 10,
-  mainAxisSpacing: 10,
-  crossAxisCount: 4,
-  children: <Widget>[
-    Container(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 4,
+      children: <Widget>[
+        const Button(value: '0'),
+        const Button(value: '1'),
+        const Button(value: '2'),
+        const Button(value: '3'),
+        const Button(value: '4'),
+        const Button(value: '5'),
+        const Button(value: '6'),
+        const Button(value: '7'),
+        const Button(value: '8'),
+        const Button(value: '9'),
+      ],
+    );
+  }
+}
+
+class Button extends StatelessWidget {
+  final String value;
+  final Color? color;
+  const Button({super.key, required this.value, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    final color = this.color ?? Theme.of(context).colorScheme.primaryContainer;
+    return Container(
       padding: const EdgeInsets.all(8),
-      color: Colors.teal[100],
-      child: const Text("He'd have you all unravel at the"),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[200],
-      child: const Text('Heed not the rabble'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[300],
-      child: const Text('Sound of screams but the'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[400],
-      child: const Text('Who scream'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[500],
-      child: const Text('Revolution is coming...'),
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      color: Colors.teal[600],
-      child: const Text('Revolution, they...'),
-    ),
-  ],
-);
+      color: color,
+      child: Center(
+        child: Text(
+          value,
+          style: Theme.of(context).textTheme.displaySmall,
+        ),
+      ),
+    );
   }
 }
