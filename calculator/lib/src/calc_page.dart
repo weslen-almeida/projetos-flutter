08c5e4a897fe4d29bb5c451f872ee2c074f28f63
Weslen Almeida <weslengomes@gmail.com>
2025-03-11 05:52:54 -0300

    fix(format): nova formatação flutter

--- calculator/lib/src/calc_page.dart
+++ calculator/lib/src/calc_page.dart
@@ -5,24 +5,21 @@ import 'package:flutter/material.dart';
 class CalcPage extends StatefulWidget {
   const CalcPage({super.key});
 
- @override
- State<CalcPage> createState() => _CalcPageState();
+  @override
+  State<CalcPage> createState() => _CalcPageState();
 }
 
 class _CalcPageState extends State<CalcPage> {
-   @override
+  @override
   Widget build(BuildContext context) {
     return const Material(
       child: Column(
         children: [
-          Expanded(
-            flex: 1,
-            child:  Display(value: '100')
-          ),
+          Expanded(flex: 1, child: Display(value: '100')),
           Expanded(
             flex: 3,
             child: ButtonHub(),
-            ),
+          ),
         ],
       ),
     );
